5b3d88e8ecee1918913c8a5d8ab3984d0d1960da
Iara Moreiras Franco <121463061+iaragab@users.noreply.github.com>
2025-09-03 20:50:39 +0200

    imagenes agragadas. proyecto lista de tareas

---
+++ second-proyect/tareas.js
@@ -0,0 +1,56 @@
+const cuaderno = document.getElementById("cuaderno")
+const btnMuestra= document.getElementById("mostrarOcultar")
+let lista = JSON.parse(localStorage.getItem("tareas")) || [] 
+
+lista.forEach(tarea => mostrarTarea(tarea))
+
+if (lista.length > 0) {
+    btnMuestra.style.display = "block"
+}
+
+function listaTareas() {
+  const input = document.getElementById("lista")
+  const ingreso = input.value.trim()
+
+  if (ingreso === "") return  
+
+  const nuevaTarea = { texto: ingreso, completado: false }
+  lista.push(nuevaTarea)
+  localStorage.setItem("tareas", JSON.stringify(lista))
+
+
+  mostrarTarea(nuevaTarea)
+
+    btnMuestra.style.display = "block"
+
+  input.value = ""
+}
+
+
+function mostrarTarea(tarea) {
+  const li = document.createElement("li")
+  li.textContent = tarea.texto
+
+  if (tarea.completado) {
+    li.classList.add("completado")
+  }
+
+  li.addEventListener("click", () => {
+    tarea.completado = !tarea.completado 
+    localStorage.setItem("tareas", JSON.stringify(lista))
+
+    li.classList.toggle("completado")
+  })
+
+  cuaderno.appendChild(li)
+}
+
+btnMuestra.addEventListener("click", () => {
+  lista.shift(); 
+  localStorage.setItem("tareas", JSON.stringify(lista));
+  cuaderno.removeChild(cuaderno.firstChild);
+
+  if (lista.length === 0) {
+    btnMuestra.style.display = "none";
+  }
+});
